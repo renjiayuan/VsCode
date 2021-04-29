d2cb0243184b0437571282da4ad06d78a8002f77
jiayuan <542538494@qq.com>
2021-04-29 10:40:49 +0800

    多文件编译
    本小结讲述了vscode进行多文件编译的方法，包括命令行，cmake文件，json文件等方法

---
+++ MyProject/build/CMakeFiles/my_cmake_swap.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_cmake_swap.dir/main.cpp.obj"
+  "CMakeFiles/my_cmake_swap.dir/swap.cpp.obj"
+  "libmy_cmake_swap.dll.a"
+  "my_cmake_swap.exe"
+  "my_cmake_swap.exe.manifest"
+  "my_cmake_swap.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_cmake_swap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
